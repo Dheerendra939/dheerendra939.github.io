323620b4a67859c46771596b9c4d13f9a18cdbb5
Dheerendra939 <126550447+Dheerendra939@users.noreply.github.com>
2025-10-09 12:36:53 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "staupsoaksy.net",
-    "zoneId": 10014367
+    "zoneId": 10014367,10014366,10014365,10014364,10014363
 }
 self.lary = ""
 importScripts('https://staupsoaksy.net/act/files/service-worker.min.js?r=sw')
